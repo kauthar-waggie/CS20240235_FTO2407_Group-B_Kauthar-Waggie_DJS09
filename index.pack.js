b0524c02369abc75d9a2ba6c8fbee059ca210efe
Kauthar Waggie <kautharwaggie1@gmail.com>
2024-11-22 20:54:28 +0200

    added images

--- index.pack.js
+++ index.pack.js
@@ -1,43 +1,112 @@
-var returningUserDisplay = document.querySelector('#returning-user');
-var userNameDisplay = document.querySelector('#user');
-var reviewTotalDisplay = document.querySelector('#reviews');
-var isOpen;
-var reviews = [
-    {
-        name: 'Sheia',
-        stars: 5,
-        loyaltyUser: true,
-        date: '01-04-2021'
-    },
-    {
-        name: 'Andrzej',
-        stars: 3,
-        loyaltyUser: false,
-        date: '28-03-2021'
-    },
-    {
-        name: 'Omar',
-        stars: 4,
-        loyaltyUser: true,
-        date: '27-03-2021'
-    },
-];
-function showReviewTotal(value, reviewer, isLoyalty) {
-    var iconDisplay = isLoyalty ? '⭐' : '';
-    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay;
-}
-showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
-var you = {
-    firstName: 'Bobby',
-    lastName: 'Brown',
-    isReturning: true,
-    age: 35,
-    stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
-};
-function populateUser(isReturning, userName) {
-    if (isReturning == true) {
-        returningUserDisplay.innerHTML = 'back';
+define("utils", ["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.showReviewTotal = showReviewTotal;
+    exports.populateUser = populateUser;
+    var reviewTotalDisplay = document.querySelector('#reviews');
+    var returningUserDisplay = document.querySelector('#returning-user');
+    var userNameDisplay = document.querySelector('#user');
+    function showReviewTotal(value, reviewer, isLoyalty) {
+        var iconDisplay = isLoyalty ? '⭐' : '';
+        reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay;
     }
-    userNameDisplay.innerHTML = userName;
-}
-populateUser(you.isReturning, you.firstName);
+    function populateUser(isReturning, userName) {
+        if (isReturning) {
+            returningUserDisplay.innerHTML = 'back';
+        }
+        userNameDisplay.innerHTML = userName;
+    }
+});
+define("index", ["require", "exports", "utils"], function (require, exports, utils_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var propertyContainer = document.querySelector('.properties');
+    var footer = document.querySelector('.footer');
+    var isOpen;
+    // Reviews
+    var reviews = [
+        {
+            name: 'Sheia',
+            stars: 5,
+            loyaltyUser: true,
+            date: '01-04-2021'
+        },
+        {
+            name: 'Andrzej',
+            stars: 3,
+            loyaltyUser: false,
+            date: '28-03-2021'
+        },
+        {
+            name: 'Omar',
+            stars: 4,
+            loyaltyUser: true,
+            date: '27-03-2021'
+        },
+    ];
+    // User
+    var you = {
+        firstName: 'Bobby',
+        lastName: 'Brown',
+        isReturning: true,
+        age: 35,
+        stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
+    };
+    // Array of Properties
+    var properties = [
+        {
+            image: 'images/colombian-shack.jpeg',
+            title: 'Colombian Shack',
+            price: 45,
+            location: {
+                firstLine: 'shack 37',
+                city: 'Bogota',
+                code: 45632,
+                country: 'Colombia'
+            },
+            contact: [+112343823978921, 'marywinkle@gmail.com'],
+            isAvailable: true
+        },
+        {
+            image: 'images/poland-cottage.jpeg',
+            title: 'Polish Cottage',
+            price: 34,
+            location: {
+                firstLine: 'no 23',
+                city: 'Gdansk',
+                code: 343903,
+                country: 'Poland'
+            },
+            contact: [+1298239028490830, 'garydavis@hotmail.com'],
+            isAvailable: false
+        },
+        {
+            image: 'images/london-flat.jpeg',
+            title: 'London Flat',
+            price: 23,
+            location: {
+                firstLine: 'flat 15',
+                city: 'London',
+                code: 35433,
+                country: 'United Kingdom',
+            },
+            contact: [+34829374892553, 'andyluger@aol.com'],
+            isAvailable: true
+        }
+    ];
+    // Functions
+    (0, utils_1.showReviewTotal)(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
+    (0, utils_1.populateUser)(you.isReturning, you.firstName);
+    // Add the properties
+    for (var i = 0; i < properties.length; i++) {
+        var card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = properties[i].title;
+        var image = document.createElement('img');
+        image.setAttribute('src', properties[i].image);
+        card.appendChild(image);
+        propertyContainer.appendChild(card);
+    }
+    var currentLocation = ['London', '11.03', 17];
+    footer.innerHTML = currentLocation[0] + ' ' + currentLocation[1] + ' ' + currentLocation[2] + '°';
+});
--- index.ts
+++ index.ts
@@ -1,49 +1,49 @@
+// Enum Types
+
 import { showReviewTotal, populateUser } from './utils'
+import { Permissions, LoyaltyUser } from './enums'
+const propertyContainer = document.querySelector('.properties')
+const footer = document.querySelector('.footer')
+
 let isOpen: boolean
 
 // Reviews
 const reviews : { 
     name: string; 
     stars: number; 
-    loyaltyUser: boolean; 
+    loyaltyUser: LoyaltyUser; 
     date: string
     }[] = [
     {
         name: 'Sheia',
         stars: 5,
-        loyaltyUser: true,
+        loyaltyUser: LoyaltyUser.GOLD_USER,
         date: '01-04-2021'
     },
     {
         name: 'Andrzej',
         stars: 3,
-        loyaltyUser: false,
+        loyaltyUser: LoyaltyUser.BRONZE_USER,
         date: '28-03-2021'
     },
     {
         name: 'Omar',
         stars: 4,
-        loyaltyUser: true,
+        loyaltyUser: LoyaltyUser.SILVER_USER,
         date: '27-03-2021'
     },
 ]
 
-// User
-const you: {
-    firstName: string;
-    lastName: string;
-    isReturning: boolean;
-    age: number;
-    stayedAt: string[]
-} = {
+const you = {
     firstName: 'Bobby',
     lastName: 'Brown',
+    permissions: Permissions.ADMIN,
     isReturning: true,
     age: 35,
     stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
 }
 
-//Properties
+// Array of Properties
 const properties : {
     image: string;
     title: string;
@@ -54,11 +54,47 @@ const properties : {
         code: number;
         country: string;
     };
-    contact: string;
+    contact: [ number, string ];
     isAvailable: boolean;
 }[] = [
     {
-        
+        image: 'images/colombia-property.jpg',
+        title: 'Colombian Shack',
+        price: 45,
+        location: {
+            firstLine: 'shack 37',
+            city: 'Bogota',
+            code: 45632,
+            country: 'Colombia'
+        },
+        contact: [+112343823978921, 'marywinkle@gmail.com'],
+        isAvailable: true  
+    },
+    {
+        image: 'images/poland-property.jpg',
+        title: 'Polish Cottage',
+        price: 34,
+        location: {
+            firstLine: 'no 23',
+            city: 'Gdansk',
+            code: 343903,
+            country: 'Poland'
+        },
+        contact: [+1298239028490830, 'garydavis@hotmail.com'],
+        isAvailable: false 
+    },
+    {
+        image: 'images/london-property.jpg',
+        title: 'London Flat',
+        price: 23,
+        location: {
+            firstLine: 'flat 15',
+            city: 'London',
+            code: 35433,
+            country: 'United Kingdom',
+        },
+        contact: [+34829374892553, 'andyluger@aol.com'],
+        isAvailable: true
     }
 ]
 
@@ -66,4 +102,18 @@ const properties : {
 showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
 
 populateUser(you.isReturning, you.firstName)
+
+// Add the properties
+for (let i = 0; i < properties.length; i++) {
+    const card = document.createElement('div')
+    card.classList.add('card')
+    card.innerHTML = properties[i].title
+    const image = document.createElement('img')
+    image.setAttribute('src', properties[i].image)
+    card.appendChild(image)
+    propertyContainer.appendChild(card)
+}
+
+let currentLocation : [string, string, number] = ['London', '11.03', 17]
+footer.innerHTML = currentLocation[0] + ' ' + currentLocation[1] + ' ' + currentLocation[2] + '°'
 
